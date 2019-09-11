616d8e35b366943f684466dcd1d883fee3454dd9
DeyviTabora <taborapaz@gmail.com>
2019-09-11 11:51:40 -0600

    Se crea el modelo de categorias

---
+++ OrangeAPI/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OrangeAPI.Models
+{
+    public class Category
+    {
+        [Key]
+        public Int32 Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public String Name { get; set; }
+
+        public Byte Image { get; set; }
+    }
+}
