ef25244eba2ce8bed61783c47345d3f3bafd8822
Gerson Caballero <gersoncaballero@unitec.edu>
2019-09-15 22:01:09 -0600

    News Controllers
    Create controller and model of bikers and products

---
+++ OrangeAPI/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OrangeAPI.Models
+{
+    public class Product
+    {
+        public int IdProduct { get; set; }
+
+        public int IdCommerce { get; set; }
+
+        [ForeignKey("IdCommerce")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte Image { get; set; }
+        public double Precio { get; set; }
+        public int Quantity { get; set; }
+    }
+}
